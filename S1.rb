4bc808e3eb2da45b01be45966e0fa6716932bd8d
Oscar Forero <oscarf@192.168.1.108>
2018-08-30 22:31:28 -0500

    Sinatra

---
+++ S1.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+
+get "/" do
+  if params[:nombre] = 0
+    <<-HTML
+        <h1>"Hola Desconocido!"</h1>
+        HTML
+  elsif params[:nombre].empty?
+    <<-HTML
+        <h1>"Hola Desconocido!"</h1>
+        HTML
+ else params[:nombre]
+    <<-HTML
+    <h1>"Hola #{params[:nombre]}!"</h1>
+    HTML
+  end
+end
