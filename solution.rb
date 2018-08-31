21bc5475fd9d80c963b7ccb760d71628b30b54d8
Oscar Forero <oscarf@192.168.1.108>
2018-08-30 23:08:17 -0500

    SR1

--- solution.rb
+++ solution.rb
@@ -1,13 +1,13 @@
 require "sinatra"
 
-get "/" do
-  if params[:nombre].empty?
-    <<-HTML
-      <h1>"Hola Desconocido!"</h1>
-    HTML
-  else params[:nombre]
-    <<-HTML
-      <h1>"Hola #{params[:nombre]}!"</h1>
-    HTML
-  end
+get '/' do
+	unless params[:nombre]
+		<<-HTML
+		<h1>"Hola desconocido!"</h1>
+		HTML
+	else
+		<<-HTML
+		<h1>"Hola #{params[:nombre]}!"</h1>
+		HTML
+	end
 end
